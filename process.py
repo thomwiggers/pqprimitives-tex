fa762d719484bc35d59ccda2d7ffb03bbdd01939
Thom Wiggers <thom@thomwiggers.nl>
2021-05-21 16:47:05 +0200

    add signature schemes

--- process.py
+++ process.py
@@ -18,6 +18,10 @@ if __name__ == "__main__":
         reader = csv.DictReader(kemsfile)
         kems = [row for row in reader]
 
+    with open("sigs.csv", "r") as sigsfile:
+        reader = csv.DictReader(sigsfile)
+        sigs = [row for row in reader]
+
     template = env.get_template("kems.tex.j2")
     with open("kems.tex", "w") as kemfile:
         kemfile.write(template.render(kems=kems))
@@ -25,3 +29,11 @@ if __name__ == "__main__":
     template = env.get_template("kemsoverview.tex.j2")
     with open("kemsoverview.tex", "w") as overviewfile:
         overviewfile.write(template.render(kems=kems))
+
+    template = env.get_template("sigs.tex.j2")
+    with open("sigs.tex", "w") as kemfile:
+        kemfile.write(template.render(sigs=sigs))
+
+    template = env.get_template("sigsoverview.tex.j2")
+    with open("sigsoverview.tex", "w") as overviewfile:
+        overviewfile.write(template.render(sigs=sigs))
